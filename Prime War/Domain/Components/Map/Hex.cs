0aeb3203c670be00630e1b8f1affe10e69c33f66
ryan@loreforgegames.com <ryan@loreforgegames.com>
2019-10-02 19:09:37 -0400

    Revert "Revert "attack tests""
    This reverts commit 51bb224f292f80ed781e81dd5947255fe7918f2b.

--- Prime War/Domain/Components/Map/Hex.cs
+++ Prime War/Domain/Components/Map/Hex.cs
@@ -4,18 +4,18 @@ namespace PrimeWarEngine.Domain.Components.Map
 {
     public class Hex
     {
-        Coordinates coordinates { get; set; }
-        FeatureType terrain { get; set; }
+        public Coordinates coordinates { get; }
+        public FeatureType terrain { get; set; }
 
         public Hex()
         {
             coordinates = Coordinates.origin;
             terrain = FeatureType.None;
         }
-        public Hex(Coordinates coords )
+        public Hex(Coordinates coords, FeatureType feature = FeatureType.None)
         {
             coordinates = coords;
-            terrain = FeatureType.None;
+            terrain = feature;
         }
     }
 }
--- PrimeWar.Tests/Dice/AttackShould.cs
+++ PrimeWar.Tests/Dice/AttackShould.cs
@@ -1,9 +1,10 @@
 ﻿using NUnit.Framework;
 using PrimeWarEngine.Domain.Components.Dice;
 using PrimeWarEngine.Domain.Controllers;
+using PrimeWarEngine.Domain.Components.Map;
 using System;
 
-namespace PrimeWar.Tests.Dice
+namespace PrimeWarEngine.Tests.Dice
 {
     [TestFixture]
     public class AttackShould
@@ -12,9 +13,9 @@ namespace PrimeWar.Tests.Dice
         public void ThrowExceptionWhenTargetOutOfRange()
         {
             DieCode dieCode = new DieCode(1, 2, 3);
-            Target t = new Target("test", 7, dieCode);
-            var sut = new Attack(new PrimeWarEngine.Domain.Components.Abilities.AttackCode(dieCode, 4, false));
-            Assert.That(() => sut.MakeAttack(t), Throws.TypeOf<ArgumentOutOfRangeException>());
+            TargetController t = new TargetController( new Target("test", 7), new Coordinates(7,0));
+            var sut = new Attack(new Domain.Components.Abilities.AttackCode(dieCode, 4, false));
+            Assert.That(() => sut.MakeAttack( new Coordinates(), t), Throws.TypeOf<ArgumentOutOfRangeException>());
         }
     }
 }
